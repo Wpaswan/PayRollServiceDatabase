35e494cafebd2fc653f28877502ae5267911e4a3
Wpaswan <dablumcapaswan@gmail.com>
2021-12-31 11:07:22 -0800

    [I modified the code to get data between two range of Id's]

--- EmployeePayrollService/EmployeeRepo.cs
+++ EmployeePayrollService/EmployeeRepo.cs
@@ -45,8 +45,8 @@ namespace EmployeePayrollService
                             employeeModel.TaxablePay = dr.GetDecimal(8);
                             employeeModel.Tax = dr.GetDecimal(9);
                             employeeModel.NetPay = dr.GetDecimal(10);
-                            employeeModel.City = dr.GetString(12);
-                            employeeModel.Country = dr.GetString(13);
+                            //employeeModel.City = dr.GetString(12);
+                            //employeeModel.Country = dr.GetString(13);
                             System.Console.WriteLine(employeeModel.EmployeeName+" "+employeeModel.BasicPay+ " "+employeeModel.StartDate +" "+ employeeModel.Gender+" "+ employeeModel.PhoneNumber+" "+employeeModel.Address+" "+ employeeModel.Department+" "+ employeeModel.Deductions+" "+ employeeModel.TaxablePay+" "+ employeeModel.Tax+" "+ employeeModel.NetPay);
                             System.Console.WriteLine("\n");
                         }
@@ -73,7 +73,7 @@ namespace EmployeePayrollService
                 using (connection)
                 {
                     //var qury=values()
-                    SqlCommand command = new SqlCommand("SpAddEmployeeDetails2", connection);
+                    SqlCommand command = new SqlCommand("SpAddEmployeeDetails1", connection);
                     command.CommandType = System.Data.CommandType.StoredProcedure;
                     command.Parameters.AddWithValue("@EmployeeName", model.EmployeeName);
                     command.Parameters.AddWithValue("@PhoneNumber", model.PhoneNumber);
@@ -86,8 +86,8 @@ namespace EmployeePayrollService
                     command.Parameters.AddWithValue("@Tax", model.Tax);
                     command.Parameters.AddWithValue("@NetPay", model.NetPay);
                     command.Parameters.AddWithValue("@StartDate", DateTime.Now);
-                    command.Parameters.AddWithValue("@City", model.City);
-                    command.Parameters.AddWithValue("@Country", model.Country);
+                    //command.Parameters.AddWithValue("@City", model.City);
+                    //command.Parameters.AddWithValue("@Country", model.Country);
                     connection.Open();
                     var result = command.ExecuteNonQuery();
                     connection.Close();
@@ -109,6 +109,59 @@ namespace EmployeePayrollService
                 connection.Close();
             }
             return false;
+        }
+        public bool RetriveDataBetweenRange()
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection(connectionString);
+                //   SqlConnection connection = new SqlConnection(connectionString);
+                EmployeeModel employeeModel = new EmployeeModel();
+                using (connection)
+                {
+                    string query = @"Select * from employee_payroll1 where Id between 2 and 10";
+                    SqlCommand cmd = new SqlCommand(query, connection);
+                    connection.Open();
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    if (dr.HasRows)
+                    {
+                        while (dr.Read())
+                        {
+                            //employeeModel.EmployeeID = dr.GetInt32(0);
+                            employeeModel.EmployeeID = dr.GetInt32(0);
+                            employeeModel.EmployeeName = dr.GetString(1);
+                            employeeModel.PhoneNumber = dr.GetString(2);
+                            employeeModel.Address = dr.GetString(3);
+                            employeeModel.BasicPay = dr.GetDecimal(6);
+                            employeeModel.StartDate = dr.GetDateTime(11);
+                            employeeModel.Gender = Convert.ToChar(dr.GetString(5));
+                            // employeeModel.PhoneNumber = dr.GetString(4);
+
+                            employeeModel.Department = dr.GetString(4);
+                            employeeModel.Deductions = dr.GetDecimal(7);
+                            employeeModel.TaxablePay = dr.GetDecimal(8);
+                            employeeModel.Tax = dr.GetDecimal(9);
+                            employeeModel.NetPay = dr.GetDecimal(10);
+                            //employeeModel.City = dr.GetString(12);
+                            //employeeModel.Country = dr.GetString(13);
+                            System.Console.WriteLine(employeeModel.EmployeeName+" "+employeeModel.BasicPay+ " "+employeeModel.StartDate +" "+ employeeModel.Gender+" "+ employeeModel.PhoneNumber+" "+employeeModel.Address+" "+ employeeModel.Department+" "+ employeeModel.Deductions+" "+ employeeModel.TaxablePay+" "+ employeeModel.Tax+" "+ employeeModel.NetPay);
+                            System.Console.WriteLine("\n");
+                        }
+                    }
+                    else
+                    {
+                        System.Console.WriteLine("No data found");
+                        return false;
+                    }
+                    return true;
+                }
+            }
+            catch (Exception e)
+            {
+                System.Console.WriteLine(e.Message);
+                return false;
+            }
+
         }
 
 
--- EmployeePayrollService/Program.cs
+++ EmployeePayrollService/Program.cs
@@ -11,34 +11,13 @@ employee.Gender = 'M';
 employee.BasicPay = 10000.00M;
 employee.Deductions = 1500;
 employee.StartDate = Convert.ToDateTime("2020-11-03");
-employee.City ="Madhubani";
-employee.Country ="India";
+//employee.City ="Madhubani";
+//employee.Country ="India";
 
 if (repo.AddEmployee(employee))
     Console.WriteLine("Records added successfully");
-employee.EmployeeName = "Dablu";
-employee.Department = "It";
-employee.PhoneNumber = "7865432123";
-employee.Address = "02-Ranchi";
-employee.Gender = 'M';
-employee.BasicPay = 10000.00M;
-employee.Deductions = 1500;
-employee.StartDate = Convert.ToDateTime("2022-04-03");
-employee.City ="Bokaro";
-employee.Country ="India";
-if (repo.AddEmployee(employee))
-    Console.WriteLine("Records added successfully");
-employee.EmployeeName = "Terissa";
-employee.Department = "Science";
-employee.PhoneNumber = "7865432123";
-employee.Address = "02-xyz";
-employee.Gender = 'F';
-employee.BasicPay = 10000.00M;
-employee.Deductions = 1500;
-employee.StartDate = Convert.ToDateTime("2022-04-03");
-employee.City="Abc";
-employee.Country ="Wxy";
-if (repo.AddEmployee(employee))
-    Console.WriteLine("Records added successfully");
+
 repo.GetAllEmployee();
+Console.WriteLine("Records beween range");
+repo.RetriveDataBetweenRange();
 Console.ReadKey();
