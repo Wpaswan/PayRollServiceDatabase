fcdef67fa2b622e8e29597517257980b55610769
Wpaswan <dablumcapaswan@gmail.com>
2022-01-03 18:20:38 -0800

    [I modified the code to add new record]

--- EmployeePayrollService/Program.cs
+++ EmployeePayrollService/Program.cs
@@ -1,36 +1,61 @@
 ﻿using EmployeePayrollService;
-
-Console.WriteLine("Welcome to Employee Payroll!");
 EmployeeRepo repo = new EmployeeRepo();
 EmployeeModel employee = new EmployeeModel();
-employee.EmployeeName = "Mohan";
-employee.Department = "Tech1";
-employee.PhoneNumber = "6302907678";
-employee.Address = "02-Patna";
-employee.Gender = 'M';
-employee.BasicPay = 10000.00M;
-employee.Deductions = 1500;
-employee.StartDate = Convert.ToDateTime("2020-11-03");
-//employee.City ="Madhubani";
-//employee.Country ="India";
-
+Console.WriteLine("Welcome to Employee Payroll!");
+Console.WriteLine("Please select any");
+Console.WriteLine("1.To add \n 2.To get details \n 3.Retrive data between range \n 4.To find sum of salary \n 5.To find average of salary" +
+    " \n 6.To find max salary \n 7.To find min salary \n 8.To count salary");
+int choice=Convert.ToInt32(Console.ReadLine());
+switch (choice) {
+    case 1:
+        Console.WriteLine("Enter employee name:");
+        employee.EmployeeName = Console.ReadLine();
+        Console.WriteLine("Enter technology");
+        employee.Department = Console.ReadLine();
+        Console.WriteLine("Enter contact number");
+        employee.PhoneNumber = Console.ReadLine();
+        Console.WriteLine("Enter address");
+        employee.Address = Console.ReadLine();
+        Console.WriteLine("Enter sex");
+        employee.Gender = Convert.ToChar(Console.ReadLine());
+        Console.WriteLine("Enter basic pay");
+        employee.BasicPay = Convert.ToInt32((Console.ReadLine() != null) ? Console.ReadLine() : 0);
+        Console.WriteLine("Enter Deduction amount");
+        employee.Deductions = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter data in format yy-mm-dd");
+employee.StartDate = Convert.ToDateTime(Console.ReadLine());
 if (repo.AddEmployee(employee))
     Console.WriteLine("Records added successfully");
-
-repo.GetAllEmployee();
-Console.WriteLine("Records beween range");
-repo.RetriveDataBetweenRange();
-Console.WriteLine("Sum:");
-var result=repo.FindSum();
-if(result!=false)
-    Console.WriteLine(result);
-Console.WriteLine("Average=");
-repo.FindAverage();
-Console.WriteLine("Maximum salary:");
-repo.FindMaxSalary();
-Console.WriteLine("Minimum Salary");
-repo.FindMinSalary();
-Console.WriteLine();
-Console.WriteLine("Salary count:");
-repo.countSalary();
+break;
+        case 2:
+        Console.WriteLine("All employee records:");
+        repo.GetAllEmployee();
+        break;
+    case 3:
+        Console.WriteLine("Records beween range");
+        repo.RetriveDataBetweenRange();
+        break;
+    case 4:
+       Console.WriteLine("Sum:");
+       var result=repo.FindSum();
+       if(result!=false)
+       Console.WriteLine(result);
+        break;
+    case 5:
+        Console.WriteLine("Average=");
+        repo.FindAverage();
+        break;
+    case 6:
+        Console.WriteLine("Maximum salary:");
+        repo.FindMaxSalary();
+        break;
+    case 7:
+         Console.WriteLine("Minimum Salary");
+         repo.FindMinSalary();
+        break;
+    case 8:
+         Console.WriteLine("Salary count:");
+         repo.countSalary();
+         break;
+}
 Console.ReadKey();
---
+++ EmployeePayrollService/SQLQuery3.sql
@@ -0,0 +1,17 @@
+﻿---------------MinimumSalary---------------------------
+create procedure FindMin
+(
+@basic_pay decimal
+)
+as 
+begin TRY 
+Select Min(basic_pay) from employee_payroll1 where  gender='F' group by gender
+End TRY
+BEGIN CATCH
+SELECT
+ERROR_NUMBER() AS ERRORNumber,
+ERROR_STATE() AS ERRORState,
+ERROR_PROCEDURE() AS ERRORProcedure,
+ERROR_LINE() AS ERRORLine,
+ERROR_MESSAGE() AS ERRORMessage;
+END CATCH
